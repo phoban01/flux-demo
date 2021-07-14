289d5a902a90c8573855ff9cb4b616f845c10731
Piaras Hoban <phoban01@gmail.com>
2021-07-14 16:32:36 +0100

    add kind start script

---
+++ scripts/create_cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create_cluster.sh
+# Version: v1.0.0
+# Author: Piaras Hoban <phoban01@gmail.com>
+
+set -o errexit
+
+cat <<EOF | kind create cluster --config -
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+name: flux-demo
+nodes:
+- role: control-plane
+- role: worker
+EOF
